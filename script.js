7bb35b918222449237557219272738ffec296af0
Zgibbs58 <zacharywgibbs@gmail.com>
2023-07-22 14:03:35 -0500

    Moved the new PromptTemplate class and promptInput into the try section of the async promptFunc.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const { OpenAI } = require("langchain/llms/openai");
 require("dotenv").config();
 const inquirer = require("inquirer");
-const { PromptTemplates } = require("langchain/prompts");
+const { PromptTemplate } = require("langchain/prompts");
 
 const model = new OpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
@@ -11,7 +11,7 @@ const model = new OpenAI({
   model: "gpt-3.5-turbo",
 });
 
-const init = () => {
+const init = async () => {
   inquirer
     .prompt([
       {
@@ -27,22 +27,20 @@ const init = () => {
 
 const promptFunc = async (input) => {
   try {
+    const prompt = new PromptTemplate({
+      //template property is where we inject the user input using \n immediately followed by curly braces surrounding a variable name.
+      template: "You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{question}",
+      //   variable name is defined in the next property, inputVariables.
+      inputVariables: ["question"],
+    });
+    const promptInput = await prompt.format({
+      question: input,
+    });
     const res = await model.call(input);
     console.log(res);
   } catch (err) {
     console.error(err);
   }
 };
-
-const prompt = new PromptTemplates({
-  //template property is where we inject the user input using \n immediately followed by curly braces surrounding a variable name.
-  template: "You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{question}",
-  //   variable name is defined in the next property, inputVariables.
-  inputVariables: ["question"],
-});
-
-const promptInput = await prompt.format({
-  question: input,
-});
 
 init();
