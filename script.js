0feca7c8881c9eb1823b7a59a2fe626b7c090d4c
Zgibbs58 <zacharywgibbs@gmail.com>
2023-07-22 14:11:43 -0500

    Implemented StructuredOutputParser to proved the user with a structured response.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const { OpenAI } = require("langchain/llms/openai");
 require("dotenv").config();
 const inquirer = require("inquirer");
 const { PromptTemplate } = require("langchain/prompts");
+const { StructuredOutputParser } = require("langchain/output_parsers");
 
 const model = new OpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
@@ -32,15 +33,24 @@ const promptFunc = async (input) => {
       template: "You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{question}",
       //   variable name is defined in the next property, inputVariables.
       inputVariables: ["question"],
+      partialVariables: { format_instructions: formatInstructions },
     });
     const promptInput = await prompt.format({
       question: input,
     });
     const res = await model.call(input);
-    console.log(res);
+    console.log(await parser.parse(res));
   } catch (err) {
     console.error(err);
   }
 };
+
+// With a `StructuredOutputParser` we can define a schema for the output.
+const parser = StructuredOutputParser.fromNamesAndDescriptions({
+  code: "Javascript code that answers the user's question",
+  explanation: "detailed explanation of the example code provided",
+});
+
+const formatInstructions = parser.getFormatInstructions();
 
 init();
